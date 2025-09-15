295149f0de75fdf74d66d1f241562e2bc50b4386
Pusrik <138851131+Pusrik@users.noreply.github.com>
2025-09-16 01:47:34 +0700

    cell02 all ex

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,11 @@
+"""cell02"""
+def main():
+    """"ex01"""
+    num = int(input())
+    if num == 0:
+        print("This number is both positive and negative.")
+    elif num < 0:
+        print("This number is negative.")
+    else:
+        print("This number is positive.")
+main()
---
+++ cell02/ex02/password.py
@@ -0,0 +1,10 @@
+"""cell02"""
+def main():
+    """"ex02"""
+    password = "Python is awesome"
+    enter = str(input())
+    if enter == password:
+        print("ACCESS GRANTED")
+    else:
+        print("ACCESS DENIED.")
+main()

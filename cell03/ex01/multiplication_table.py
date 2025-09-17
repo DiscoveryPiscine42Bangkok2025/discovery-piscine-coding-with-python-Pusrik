7168c9034b593a5eca1a8db794faaf1227de8971
Pusrik <138851131+Pusrik@users.noreply.github.com>
2025-09-17 23:56:02 +0700

    Update multiplication_table.py

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,9 +1,10 @@
 """cell03"""
 def main():
     """"ex01"""
-    num = int(input("Enter a number5 \n"))
+    num = int(input("Enter a number \n"))
     i = 0
     while i <= 9:
         print(f"{i} x {num} = {i*num}")
         i += 1
 main()
+
